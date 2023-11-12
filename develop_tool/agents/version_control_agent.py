2364e108036ad866344d8cbd569c37eb158c83e8
beyond-repair <132061760+beyond-repair@users.noreply.github.com>
2023-11-11 22:56:19 -0600

    Update version_control_agent.py

--- develop_tool/agents/version_control_agent.py
+++ develop_tool/agents/version_control_agent.py
@@ -5,14 +5,26 @@ class VersionControlAgent:
         self.repository_path = repository_path
 
     def initialize_repository(self):
-        subprocess.run(['git', 'init'], cwd=self.repository_path)
+        try:
+            subprocess.run(['git', 'init'], cwd=self.repository_path, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error initializing repository: {e}")
 
     def commit_changes(self, message):
-        subprocess.run(['git', 'add', '.'], cwd=self.repository_path)
-        subprocess.run(['git', 'commit', '-m', message], cwd=self.repository_path)
+        try:
+            subprocess.run(['git', 'add', '.'], cwd=self.repository_path, check=True)
+            subprocess.run(['git', 'commit', '-m', message], cwd=self.repository_path, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error committing changes: {e}")
 
     def create_branch(self, branch_name):
-        subprocess.run(['git', 'branch', branch_name], cwd=self.repository_path)
+        try:
+            subprocess.run(['git', 'branch', branch_name], cwd=self.repository_path, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating branch: {e}")
 
     def switch_branch(self, branch_name):
-        subprocess.run(['git', 'checkout', branch_name], cwd=self.repository_path)
+        try:
+            subprocess.run(['git', 'checkout', branch_name], cwd=self.repository_path, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error switching branch: {e}")
