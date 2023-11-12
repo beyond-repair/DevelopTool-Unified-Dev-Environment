cb94f44e14a7f0d6ad810b7296ca701af9351408
beyond-repair <132061760+beyond-repair@users.noreply.github.com>
2023-11-11 22:38:30 -0600

    Update and rename IDE Agent to develop_tool/agents/ide_agent.py

--- develop_tool/agents/ide_agent.py
+++ develop_tool/agents/ide_agent.py
@@ -1,4 +1,3 @@
-# develop_tool/agents/ide_agent.py
 class IDEAgent:
     def integrate_with_ide(self, ide_name):
         print(f"Integrating with {ide_name} for code editing, debugging, and running.")
